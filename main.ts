faceda59a46f97a01d49c77369d3e5ac48d8c97c
lakyung <90668130+lakyung@users.noreply.github.com>
2021-09-16 12:48:11 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
-let 거리 = 0
 input.onButtonPressed(Button.A, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
 })
+pins.digitalWritePin(DigitalPin.P8, 0)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    거리 = 340 * pins.pulseIn(DigitalPin.P1, PulseValue.High) / 1000 / 2
-    basic.showNumber(거리)
+	
+})
+basic.forever(function () {
+    if (input.temperature() >= 30) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    }
+    basic.showNumber(input.temperature())
 })
